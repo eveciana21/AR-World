6b6972a679026601c0bc162630d610135bec82d2
Eveciana21 <ericanddaisy@Erics-iMac.mynextlight.net>
2024-02-16 12:33:48 -0700

    added examinable scripts

---
+++ Assets/Scripts/Examinable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Examinable : MonoBehaviour
+{
+	private ExaminableManager _examinableManager;
+	public float examinedScaleOffset = 1f;
+
+	void Start()
+	{
+		_examinableManager = FindObjectOfType<ExaminableManager>();
+	}
+
+	public void RequestExamine()
+	{
+		_examinableManager.PerformExamine(this);
+	}
+	public void RequestUnexamine()
+	{
+		_examinableManager.PerformUnexamine();
+	}
+
+}
