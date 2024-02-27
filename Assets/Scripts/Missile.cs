71fbf4bf4463a41e1a44d2329314f852557927bb
Eveciana21 <ericanddaisy@Erics-iMac.mynextlight.net>
2024-02-26 17:36:17 -0700

    added tank and tank fire

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+   [SerializeField] private float _speed = 5;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        Destroy(this.gameObject, 3f);
+    }
+}
---
+++ Assets/Scripts/TankFire.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankFire : MonoBehaviour
+{
+    [SerializeField] private GameObject _missilePrefab;
+    [SerializeField] private GameObject _gunTransform;
+    [SerializeField] private GameObject _explosion;
+
+    public void LaunchMissile()
+    {
+        Instantiate(_missilePrefab, _gunTransform.transform.position, _gunTransform.transform.rotation);
+        GameObject explosion = Instantiate(_explosion, _gunTransform.transform.position, Quaternion.identity);
+        Destroy(explosion, 3);
+    }
+}
