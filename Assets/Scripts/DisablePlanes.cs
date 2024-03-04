302321ab7a24789c2fc33b001f928ec63185908f
Eveciana21 <ericanddaisy@Erics-iMac.mynextlight.net>
2024-03-03 17:10:29 -0700

    added title screen, added disable and enable planes

---
+++ Assets/Scripts/DisablePlanes.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class DisablePlanes : MonoBehaviour
+{
+    private ARPlaneManager _planeManager;
+
+    void Start()
+    {
+        _planeManager = GetComponent<ARPlaneManager>();
+    }
+
+    public void EnablePlaneDisplay(bool display)
+    {
+        _planeManager.enabled = display;
+        foreach(var plane in _planeManager.trackables)
+        {
+            plane.gameObject.SetActive(display);
+        }
+    }
+
+}
--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
    [SerializeField] private float _speed = 5;
-    //[SerializeField] private GameObject _explosion;
-
-   /* private void Start()
-    {
-        GameObject explosion = Instantiate(_explosion, transform.position, Quaternion.identity);
-        Destroy(explosion, 2f);
-    }*/
 
     private void Update()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-        Destroy(this.gameObject, 3f);
+        Destroy(this.gameObject, 5f);
     }
 }
